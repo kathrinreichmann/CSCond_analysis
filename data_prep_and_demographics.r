2567c02f5c6940fa490d202edbb2cd33618fb0ce
kathrinreichmann <73654814+kathrinreichmann@users.noreply.github.com>
2021-01-12 17:49:52 +0100

    update direct and indirect

--- data_prep_and_demographics.r
+++ data_prep_and_demographics.r
@@ -1,16 +1,28 @@
-#setwd("C:/Users/reich/Documents/GitHub/FLO_replication/CScond_online/data")
-setwd("\\\\sn00.zdv.uni-tuebingen.de/siskr01/Documents/Github/CSCond_online/data")
+setwd("C:/Users/reich/Documents/GitHub/FLO_replication/CScond_online/data")
+#setwd("\\\\sn00.zdv.uni-tuebingen.de/siskr01/Documents/Github/CSCond_online/data")
 
 
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
 
+##### import: 
+#single data files
+
+##### export:
+#direct.csv
+#indirect.csv
+
+
+# read files --------------------------------------------------------------
+
+
 filenames <- dir()
 dat <- data.frame()
 for (i in filenames){
   dat <- rbind(dat, read.table(i, header = TRUE, sep = ",", encoding = "UTF-8"))
 }
+rm (filenames, i)
 unique(dat$subject)
 
 comments_one <- dat$responses[dat$trial_type == "survey-text" & dat$condition == "one_one"]
@@ -27,41 +39,42 @@ dat$response <- as.numeric(dat$response)
 dat$responses <- as.numeric(dat$responses)
 dat$rt <- as.numeric(dat$rt)
 
+
+# exclude participants ----------------------------------------------------
+
+
 #chinese speaking participants
 #0 = ja
 get_tables <- aggregate(response ~ subject + chinese, dat, mean)
 table(get_tables$chinese)
-dat <- dat[!dat$chinese == "0",]
+dat <- dat[!dat$chinese == 0,]
 
 #participants who did not pay attention
 get_tables <- aggregate(response ~ subject + hinschauen, dat, mean)
 table(get_tables$hinschauen) #1 = nicht hinschauen
-dat <- dat[!dat$hinschauen == "1",]
-#table(dat$hinschauen)
+dat <- dat[!dat$hinschauen == 1,]
 
 #who were distracted
 get_tables <- aggregate(response ~ subject + ablenkung, dat, mean)
 table(get_tables$ablenkung) # 0 = abgelenkt
-dat <- dat[!dat$ablenkung == "0",]
-#table(dat$ablenkung)
+dat <- dat[!dat$ablenkung == 0,]
 
 #who did not follow instructions
 get_tables <- aggregate(response ~ subject + gewissenhaft, dat, mean)
 table(get_tables$gewissenhaft) # 1 = nicht gewissenhaft
 
-#always clicked the same answer in direct measure
-subjects <- ggplot(dat, aes (x= subject, y = response, col = subject)) +
-                geom_point(show.legend = FALSE)
-#subjects
-
 #answered too slowly in indirect measure (rt > 3 SDs)
 exclude <- aggregate(rt ~ subject, dat, mean)
-exclusion_sd <- sd(na.omit(dat$rt))*3
-exclusion_sd
-exclude[exclude$rt > exclusion_sd,]
+#exclusion_sd <- sd(na.omit(dat$rt))*3
+#exclusion_sd
+#exclude[exclude$rt > exclusion_sd,]
+
+#exclude data with NA as subject (1 VP)
+dat <- dat[!is.na(dat$subject == TRUE),]
 
 #total after excluding participants
 length(unique(dat$subject))
+
 
 #condition
 get_tables <- aggregate(response ~ subject + condition, dat, mean)
@@ -82,6 +95,10 @@ as.factor(gsub(".png$", "", dat$target))-> dat$target
 as.factor(gsub("targets/", "", dat$target))-> dat$target
 as.factor(gsub(".png$", "", dat$cs_selected))-> dat$cs_selected
 as.factor(gsub("category", "", dat$cs_selected))-> dat$cs_selected
+
+
+# demographics ------------------------------------------------------------
+
 
 demographics <- dat[c('subject', 'age', 'gender', 'education', 'chinese', 'handedness')]
 demographics <- aggregate(. ~ subject, demographics, median);
@@ -105,6 +122,10 @@ table(demographics$gender)
 
 summary(demographics$age)
 
+
+# export for direct measure analysis --------------------------------------
+
+
 #extract relevant columns
 direct <- dat[dat$task == "direct" | dat$task == "all",] 
 direct <- direct[c('response', 'subject', 'condition', 'condition_code', 'measure', 'measure_code', 'val', 'type', 'type_specific', 'category', 'cs_selected')]
@@ -113,16 +134,20 @@ for (factor in as_factor){
   direct[, factor] <- as.factor(direct[,factor])
 }
 
-direct$type_specific <- factor(direct$type_specific, levels = c('CS', 'GSold', 'GSnew', 'abstract', 'all'))
+direct$type_specific <- factor(direct$type_specific, levels = c('CS', 'GSold', 'GSnew', 'abstract', 'all'), labels = c('CS', 'GS same', 'GS different', "Feature", "Group"))
 direct$response <- as.numeric(direct$response)
-#str(direct)
+str(direct)
 
 unique(direct$subject)
 
 #export files
 #setwd("\\\\sn00.zdv.uni-tuebingen.de/siskr01/Documents/Github/CSCond_analysis/CSCond_analysis/data")
-#setwd("C:/Users/reich/Documents/GitHub/CSCond_analysis")
-#write.csv2(direct, file = 'direct.csv')
+setwd("C:/Users/reich/Documents/GitHub/CSCond_analysis/data")
+write.csv2(direct, file = 'direct.csv')
+
+
+# export for indirect measure analysis ------------------------------------
+
 
 #extract relevant columns
 indirect <- dat[dat$task == "indirect",]
@@ -139,9 +164,9 @@ indirect$nr_pres <- as.numeric(indirect$nr_pres)
 indirect$rt <- as.numeric(indirect$rt)
 str(indirect)
 
-indirect$type_specific <- factor(indirect$type_specific, levels = c('CS', 'GSold', 'GSnew', 'abstract'))
+indirect$type_specific <- factor(indirect$type_specific, levels = c('CS', 'GSold', 'GSnew', 'abstract'), labels = c('CS', 'GS same', 'GS different', "Feature"))
 
 #export files
 #setwd("\\\\sn00.zdv.uni-tuebingen.de/siskr01/Documents/Github/CSCond_analysis/CSCond_analysis/data")
-#setwd("C:/Users/reich/Documents/GitHub/CSCond_analysis")
-#write.csv2(indirect, file = 'indirect.csv')
+setwd("C:/Users/reich/Documents/GitHub/CSCond_analysis/data")
+write.csv2(indirect, file = 'indirect.csv')
