1ee45410775ce4da3fd75723dedbf0a93891c88e
kathrinreichmann <73654814+kathrinreichmann@users.noreply.github.com>
2021-01-13 17:15:35 +0100

    updated anyalysis for indirect

--- indirect_measure.r
+++ indirect_measure.r
@@ -53,41 +53,186 @@ for (factor in as_factor){
   indirect[, factor] <- as.factor(indirect[,factor])
 }
 
-indirect$type_specific <- factor(indirect$type_specific, levels = c("CS", "GS same", "GS different", "Feature", "Group"))
+indirect$type_specific <- factor(indirect$type_specific, levels = c("CS", "GS same", "GS different", "Feature"))
 str(indirect)
 
+# calculate probabilities -------------------------------------------------
 
-#generate variabile prob
+
+#generate variable prob
 dataIndirect <- aggregate(response ~ subject + condition_code + condition + measure + val + type + type_specific, indirect, sum )
 dataIndirect$nr_val <- aggregate(response ~ subject + condition_code + condition + measure + val + type + type_specific, indirect, length)[[8]]
-dataIndirect$prob <- (dataIndirect$response/dataIndirect$nr_val) #prob: proportion "angenehm" of positive / negative stimuli in the respective condition
+dataIndirect$prob <- (dataIndirect$response/dataIndirect$nr_val) #prob: proportion "angenehm" of all stimuli in a combination of conditions
 head(dataIndirect)
 
 #generate variable diff
-for (subj in unique(dataIndirect$subject)){
-  for (name in unique(dataIndirect$type_specific)){
-    temp <- dataIndirect[dataIndirect$subject == subj & dataIndirect$type_specific == name,];
-    dataIndirect$diff[dataIndirect$subject == subj & dataIndirect$type_specific == name] <- temp$prob[temp$val == "pos"] - temp$prob[temp$val == "neg"]
-  }
+for (name in unique(dataIndirect$type_specific)){
+  temp <- dataIndirect[dataIndirect$type_specific == name,];
+  dataIndirect$diff[dataIndirect$type_specific == name] <- temp$prob[temp$val == "pos"] - temp$prob[temp$val == "neg"]
 }
 
+#columns for all "angenehm" and "unangenehm" answers
 dataIndirect$an <- dataIndirect$response #column for "unangenehm" answer
 dataIndirect$un <- dataIndirect$nr_val - dataIndirect$response #column for "angenehm" answer
 head(dataIndirect)
 
+
+### Plot difference scores: CS Variability x Type
+
 barplotDiffData <- aggregate(diff ~ condition + type_specific, dataIndirect, mean)
 barplotDiffData$se <- aggregate(diff ~ condition + type_specific, dataIndirect, se)[[3]]
+
 
 barplotDiff <- ggplot(barplotDiffData, aes (x = type_specific, y = diff, fill = condition)) +
   geom_bar(stat = 'identity', position = position_dodge(), show.legend = TRUE) +
   geom_errorbar(aes(ymin= diff - se, ymax= diff + se), width=.2,
                 position=position_dodge(.9)) +
-  ggtitle("Differences") + 
-  theme(plot.title = element_text (hjust = 0.5, face = "bold", size = 12)) +
+  ggtitle("Indirect Measure") + 
   scale_fill_brewer(palette = "Paired") +
-  scale_y_continuous (name = "[Prob. 'angenehm' bei Pos] - [Prob. 'angenehm' bei Neg]", breaks = seq(-0.1, 0.3, 0.1), limits = c(-0.1, 0.3))
+  theme_classic() +
+  theme(plot.title = element_text (hjust = 0.5, face = "bold", size = 12)) +
+  labs(fill = "CS Variability") +
+  scale_y_continuous (name = "[Prob. 'an' | pos] - [Prob. 'an' | neg]", breaks = seq(0, 0.3, 0.1), limits = c(0, 0.3))
 barplotDiff
 
+
+################### one-sample t-tests
+#H0: mu = 0
+#H1: mu not equal to 0 -> EC effect is significant
+
+#for GS different
+t.test(dataIndirect$diff[dataIndirect$condition == "one_one" & dataIndirect$type_specific == "GS different"], mu = 0, alternative = "two.sided")
+t.test(dataIndirect$diff[dataIndirect$condition == "many_one" & dataIndirect$type_specific == "GS different"], mu = 0, alternative = "two.sided")
+
+
+################### (1) CS Variability x Type (CS vs. GS same)
+
+#Hypothesis: interaction type x CS Variability
+
+
+CSGSsame <- dataIndirect[dataIndirect$type_specific == "CS" | dataIndirect$type_specific == "GS same",]
+
+#### mixed-model ANOVA with CS Variability x Type (CS vs. GS same)
+indirect.type <- aggregate(diff ~ condition + type_specific + subject, CSGSsame, mean)
+aov_specific <- aov_car(diff ~ condition*type_specific + Error(subject/type_specific), indirect.type, anova_table = list("pes"))
+aov_specific
+
+#2-way interaction n.s.
+
+################### CS Variability x Type (CS vs. GS same vs. GS different vs. Feature)
+
+#### mixed-model ANOVA with CS Variability x Type (CS vs. GS same vs. GS different vs. Feature)
+indirect.type <- aggregate(diff ~ condition + type_specific + subject, dataIndirect, mean)
+aov_specific <- aov_car(diff ~ condition*type_specific + Error(subject/type_specific), indirect.type, anova_table = list("pes"))
+summary(aov_specific)
+#Departure from Sphericity, use Greenhouse-Geisser correction
+
+#interaction n.s.
+#main effect type_specific
+aggregate(diff ~ type_specific, dataIndirect, mean)
+
+
+##????? as linear model
+lm.type_specific <- lm(diff ~ type_specific*condition, dataIndirect)
+summary(lm.type_specific)
+
+lm.type_specific2 <- lmer(diff ~ type_specific + condition + (type_specific | subject), dataIndirect)
+summary(lm.type_specific)
+
+lm.type_specific3 <- lmer(diff ~ type_specific*condition + (type_specific | subject), dataIndirect)
+summary(lm.type_specific3)
+
+anova(lm.type_specific2, lm.type_specific3)
+
+
+
+#simple slopes
+#CS
+lm1 <- lm(diff ~ condition, dataIndirect[dataIndirect$type_specific == "CS",])
+summary(lm1)
+
+lm2 <- lm(diff ~ condition, dataIndirect[dataIndirect$type_specific == "GS same",])
+summary(lm2)
+
+lm3 <- lm(diff ~ condition, dataIndirect[dataIndirect$type_specific == "GS different",])
+summary(lm3)
+
+lm4 <- lm(diff ~ condition, dataIndirect[dataIndirect$type_specific == "Feature",])
+summary(lm4)
+
+
+################### randomly select 2 CSs in "many-to-one"
+
+new_one <- indirect[indirect$condition == "one_one",]
+new_many <- indirect[indirect$condition == "many_one",]
+new_many <- new_many[!new_many$type_specific == "CS",]
+new_indirect <- rbind(new_one, new_many)
+
+for (subject in unique(indirect$subject)){
+  if (indirect$condition[indirect$subject == subject] == "many_one"){
+    for (cat in 1:4){
+      temp <- indirect[indirect$subject == subject & indirect$type_specific == "CS" & indirect$category == cat,]
+      select <- temp[1:2,]
+      new_indirect <- rbind(new_indirect, select)
+    }
+  }
+  
+}
+
+#generate variable prob
+dataIndirect_new <- aggregate(response ~ subject + condition_code + condition + measure + val + type + type_specific, new_indirect, sum )
+dataIndirect_new$nr_val <- aggregate(response ~ subject + condition_code + condition + measure + val + type + type_specific, new_indirect, length)[[8]]
+dataIndirect_new$prob <- (dataIndirect$response/dataIndirect$nr_val) #prob: proportion "angenehm" of all stimuli in a combination of conditions
+head(dataIndirect)
+
+#generate variable diff
+for (name in unique(dataIndirect_new$type_specific)){
+  temp <- dataIndirect_new[dataIndirect_new$type_specific == name,];
+  dataIndirect_new$diff[dataIndirect_new$type_specific == name] <- temp$prob[temp$val == "pos"] - temp$prob[temp$val == "neg"]
+}
+
+#Plot
+barplotDiffData <- aggregate(diff ~ condition + type_specific, dataIndirect_new, mean)
+barplotDiffData$se <- aggregate(diff ~ condition + type_specific, dataIndirect_new, se)[[3]]
+
+
+barplotDiff <- ggplot(barplotDiffData, aes (x = type_specific, y = diff, fill = condition)) +
+  geom_bar(stat = 'identity', position = position_dodge(), show.legend = TRUE) +
+  geom_errorbar(aes(ymin= diff - se, ymax= diff + se), width=.2,
+                position=position_dodge(.9)) +
+  ggtitle("Indirect Measure") + 
+  scale_fill_brewer(palette = "Paired") +
+  theme_classic() +
+  theme(plot.title = element_text (hjust = 0.5, face = "bold", size = 12)) +
+  labs(fill = "CS Variability") +
+  scale_y_continuous (name = "[Prob. 'an' | pos] - [Prob. 'an' | neg]", breaks = seq(0, 0.3, 0.1), limits = c(0, 0.3))
+barplotDiff
+
+#### mixed-model ANOVA with CS Variability x Type (CS -only 2 CSs vs. GS same vs. GS different vs. Feature)
+indirect.type <- aggregate(diff ~ condition + type_specific + subject, dataIndirect_new, mean)
+aov_specific <- aov_car(diff ~ condition*type_specific + Error(subject/type_specific), indirect.type, anova_table = list("pes"))
+summary(aov_specific)
+#Departure from Sphericity, use Greenhouse-Geisser correction
+
+################### calculate Generalized Linear Model (GLM)
+
+#GLM on (un, an) condition * type_specific * val
+
+glm1 <- glm(cbind(un, an) ~ 1, binomial, dataIndirect) #minimal model
+glm2 <- glm(cbind(un, an) ~ condition + type_specific + val, binomial, dataIndirect)
+glm3 <- glm(cbind(un, an) ~ condition* type_specific + val, binomial, dataIndirect)
+glm4 <- glm(cbind(un, an) ~ condition* type_specific * val, binomial, dataIndirect) #saturated model
+anova(glm1, glm2, glm3, glm4, test = "LRT")
+summary(glm4)
+
+#"WK angenehm bei one_one und CS"
+predict(glm4, data.frame(condition = "one_one", type_specific = "CS", val = 'neg'), type = "resp")
+predict(glm4, data.frame(condition = "one_one", type_specific = "CS", val = 'pos'), type = "resp")
+
+
+################## Additional plots
+
+#plot different valences
 barplotDiffData <- aggregate(prob ~ val + type_specific + condition, dataIndirect, mean)
 barplotDiffData$se <- aggregate(prob ~ val + type_specific + condition, dataIndirect, se)[[4]]
 
@@ -103,7 +248,7 @@ barplotPosNeg <- ggplot(barplotDiffData, aes (x = type_specific, y = prob, fill 
   scale_y_continuous(name = "Prob. of 'Angenehm'")
 barplotPosNeg
 
-
+#boxplot
 posnegIndirectData <- aggregate(prob ~ val + type_specific + condition, dataIndirect, mean)
 posnegIndirect <- ggplot(dataIndirect, aes (x = type_specific, y = prob, fill = condition)) +
   facet_grid(. ~ val) +
@@ -115,35 +260,50 @@ posnegIndirect <- ggplot(dataIndirect, aes (x = type_specific, y = prob, fill = 
   scale_y_continuous(name = "Prob. of 'Angenehm'")
 posnegIndirect
 
+################## (2) Variability x Type x Measure
 
-indirect.type <- aggregate(diff ~ condition + type + subject, dataIndirect[dataIndirect$type_specific == "CS" | dataIndirect$type_specific == "GSold",], mean)
-aov_specific <- aov_car(diff ~ condition*type + Error(subject/type), indirect.type, anova_table = list("pes"))
-#aov_specific
-#summary(aov_specific)
+#Plot
+barplotDiffMeasure <- aggregate(diff ~ type_specific + measure + condition, dataIndirect, mean)
+barplotDiffMeasure$se <- aggregate(diff ~ type_specific + measure + condition, dataIndirect, se)[[4]]
 
-indirect.type_specific <- aggregate(diff ~ condition + type_specific + subject, dataIndirect, mean)
-aov_specific2 <- aov_car(diff ~ condition*type_specific + Error(subject/type_specific), indirect.type_specific, anova_table = list("pes"))
-aov_specific2
-summary(aov_specific2)
+indirect.Measure <- ggplot(barplotDiffMeasure, aes (x = type_specific, y = diff, fill = condition)) +
+  facet_grid(. ~ measure) +
+  geom_bar(stat = 'identity', position = position_dodge(), show.legend = TRUE) +
+  geom_errorbar(aes(ymin= diff - se, ymax= diff + se), width=.2,
+                position=position_dodge(.9)) +
+  ggtitle("Indirect Measure") + 
+  theme(plot.title = element_text (hjust = 0.5, face = "bold", size = 12)) +
+  scale_fill_brewer(palette = "Paired")  + 
+  scale_y_continuous(name = "[Prob. 'an' | pos] - [Prob. 'an' | neg]")
+indirect.Measure
 
-#main effect type_specific
-aggregate(diff ~ type_specific, dataIndirect, mean)
-
-lm.type_specific <- lm(diff ~ type_specific*condition, dataIndirect)
-#summary(lm.type_specific)
-
-lm.type_specific2 <- lmer(diff ~ type_specific*condition + (1 | subject), dataIndirect)
-#summary(lm.type_specific)
-
-lm.type_specific3 <- lmer(diff ~ type_specific*condition + (type | subject), dataIndirect)
-summary(lm.type_specific3)
-
-anova(lm.type_specific2, lm.type_specific3)
-
+#ANOVA
 indirect.measure <- aggregate(diff ~ condition + measure + type_specific + subject, dataIndirect, mean)
 aov_measure <- aov_car(diff ~ condition*type_specific*measure + Error(subject/type_specific), indirect.measure, anova_table = list("pes"))
-#aov_measure
-#summary(aov_measure)
+aov_measure
+summary(aov_measure)
+#interaction n.s., no significant influence of 'measure'
+
+################## (3) including category
+
+categoryIndirect <- aggregate(response ~  condition  + val + type_specific + category, indirect, sum )
+categoryIndirect$nr_val <- aggregate(response ~ condition + val + type_specific + category, indirect, length)[[5]]
+categoryIndirect$prob <- (categoryIndirect$response/categoryIndirect$nr_val)
+#head(categoryIndirect)
+
+plot.categories <- ggplot(categoryIndirect, aes (x = category, y = prob, fill = val)) +
+  stat_boxplot(geom = "errorbar") +
+  geom_boxplot(show.legend = TRUE) +
+  geom_hline(yintercept = 0.5, color = "red") +
+  ggtitle("Categories") + 
+  theme(plot.title = element_text (hjust = 0.5, face = "bold", size = 12)) +
+  scale_fill_brewer(palette = "Blues") 
+plot.categories
+
+################## (4) including individual CSs
+
+################## (5) including targets
+
 
 targetsIndirect <- aggregate(response ~ target + val + condition + type_specific, indirect, sum)
 targetsIndirect$nr_val <- aggregate(response ~ target + val + condition + type_specific, indirect, length) [[5]]
@@ -180,33 +340,8 @@ plotTarget <- ggplot(plot.targets[plot.targets$val == "neg",], aes (x = target, 
   scale_y_continuous(name = "Difference (Prob. Angenehm | Pos) - (Prob Angenehm | Neg)")
 #plotTarget
 
-#GLM on (un, an)
-glm1 <- glm(cbind(un, an) ~ 1, binomial, dataIndirect) #minimal model
-glm2 <- glm(cbind(un, an) ~ condition + type_specific + val, binomial, dataIndirect)
-glm3 <- glm(cbind(un, an) ~ condition* type_specific + val, binomial, dataIndirect)
-glm4 <- glm(cbind(un, an) ~ condition* type_specific * val, binomial, dataIndirect) #saturated model
-anova(glm1, glm2, glm3, glm4, test = "LRT")
-summary(glm4)
 
-#main effect condition
-predict(glm4, data.frame(condition = "one_one", type_specific = "CS", val = 'neg'), type = "resp")
-predict(glm4, data.frame(condition = "one_one", type_specific = "GSnew", val = 'pos'), type = "resp")
-#"WK angenehm bei one_one und CS"
-
-barplotDiffMeasure <- aggregate(prob ~ val + type_specific + measure, dataIndirect, mean)
-barplotDiffMeasure$se <- aggregate(prob ~ val + type_specific + measure, dataIndirect, se)[[4]]
-
-indirect.Measure <- ggplot(barplotDiffMeasure, aes (x = type_specific, y = prob, fill = measure)) +
-  facet_grid(. ~ val) +
-  geom_bar(stat = 'identity', position = position_dodge(), show.legend = TRUE) +
-  geom_errorbar(aes(ymin= prob - se, ymax= prob + se), width=.2,
-                position=position_dodge(.9)) +
-  ggtitle("Measure") + 
-  theme(plot.title = element_text (hjust = 0.5, face = "bold", size = 12)) +
-  scale_fill_brewer(palette = "Paired")  + 
-  scale_y_continuous(name = "Prob. of 'Angenehm'")+
-    geom_hline(yintercept = 0.5, color = "red")
-indirect.Measure
+################## (6) including presentation order
 
 presOrder <- aggregate(response ~ nr_pres + val , indirect, sum)
 presOrder$nr_val <- aggregate(response ~ nr_pres + val, indirect, length) [[3]]
@@ -236,36 +371,9 @@ plot.presOrder <- ggplot(presOrder, aes (x = type_specific, y = rt, col = condit
   scale_y_continuous(name = "Presentation Order")
 plot.presOrder
 
-categoryIndirect <- aggregate(response ~  condition  + val +type_specific + category, indirect, sum )
-categoryIndirect$nr_val <- aggregate(response ~ condition + val + type_specific + category, indirect, length)[[5]]
-categoryIndirect$prob <- (categoryIndirect$response/categoryIndirect$nr_val)
-#head(categoryIndirect)
 
-plot.categories <- ggplot(categoryIndirect, aes (x = category, y = prob, fill = val)) +
-  stat_boxplot(geom = "errorbar") +
-  geom_boxplot(show.legend = TRUE) +
-  geom_hline(yintercept = 0.5, color = "red") +
-  ggtitle("Categories") + 
-  theme(plot.title = element_text (hjust = 0.5, face = "bold", size = 12)) +
-  scale_fill_brewer(palette = "Blues") 
-plot.categories
+# reaction times ----------------------------------------------------------
 
-temp <- direct[!direct$type_specific == "all",]
-indirect$responseDirect <- temp$response
-indirect$responseIndirect <- as.factor(indirect$response)
 
-#predict direct responses with answers from indirect responses: for positive pairings
 
-#plot(responseDirect ~ responseIndirect, indirect[indirect$val == "pos",])
-lm.pos <- lm(responseDirect ~ responseIndirect,indirect[indirect$val == "pos",])
-#summary(lm.pos)
 
-#predict direct responses with answers from indirect responses: for positive pairings
-
-#plot(responseDirect ~ responseIndirect, indirect[indirect$val == "neg",])
-lm.neg <- lm(responseDirect ~ responseIndirect,indirect[indirect$val == "neg",])
-#summary(lm.neg)
-
-#calculate correlations
-cor(indirect$responseDirect[indirect$val == "pos"], indirect$response[indirect$val == "pos"])
-cor(indirect$responseDirect[indirect$val == "neg"], indirect$response[indirect$val == "neg"])
